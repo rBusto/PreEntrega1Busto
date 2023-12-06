6840d306e95c8e879357a73055f2f39db17e8d79
Ramiro Busto <ramirobus81@gmail.com>
2023-12-06 03:43:21 -0300

    ultimo commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Portada from './components/portada/Portada';
+import Navbar from './components/Navbar/Navbar';
+import Inicio from './components/paginas/Inicio';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+        <Portada />
+        <Navbar />
+        <Inicio />
+        <ItemListContainer texto='Ramiro' />
+
+
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      <MostrarNombre />
+      </Router>
   );
+}
+
+const MostrarNombre = () => {
+  return <h1>Ramiro</h1>
 }
 
 export default App;
---
+++ src/components/paginas/Inicio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Carousel from '../carousel/Carousel'
+
+const Inicio = () => {
+    return (
+        <div>   
+            <Carousel />
+            <h1>Esta es la pagina de Inicio!</h1>
+
+        </div>
+    )
+}
+
+export default Inicio
